8506244524d360921e3881910c914748fbf6dd31
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2023-12-22 18:56:09 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -20,9 +20,10 @@ input_budget = []
 
 # For each selected option, display a row with the option and a text input
 for i, option in enumerate(input_silos):
-    col1, col2, _ = st.columns([1,3,2])  # Adjust the ratio as needed
+    col1, col2, col3, _ = st.columns([1,0.1,2.9,2])  # Adjust the ratio as needed
     col1.markdown(f"<div style='text-align: center; color: white; padding-top: 30px; font-size:18px;'>{option}</div>", unsafe_allow_html=True)
-    text_input_slot = col2.empty()
+    col2.markdown(f"<div style='text-align: right; color: black; padding-top: 25px; font-size: 14px;'>$</div>", unsafe_allow_html=True)
+    text_input_slot = col3.empty()
     user_input = text_input_slot.text_input('', '', key=f'input_{i}')
     input_budget.append(user_input)
 
