3d3b6f32969245b9a29d577eab10f49ca1b39096
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2023-12-24 22:21:15 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -191,11 +191,11 @@ if st.session_state.predict_leads:
     if 'user_role' not in st.session_state:
         st.session_state.user_role = ''
     
-    col_name,col_name_input,col_role,col_role_input = st.columns([0.5,2,0.5,2]) 
-    col_name.markdown(f"<div style='text-align: center; color: white; padding-top: 32px; font-size:18px;'>Company</div>", unsafe_allow_html=True)
+    col_name,col_name_input,col_role,col_role_input = st.columns([0.5,1.7,0.5,2]) 
+    col_name.markdown(f"<div style='text-align: center; color: white; padding-top: 32px; font-size:18px;'>Company:</div>", unsafe_allow_html=True)
     name_input_slot = col_name_input.empty()
     st.session_state.company_name = name_input_slot.text_input('', '', key="Company_name")
-    col_role.markdown(f"<div style='text-align: center; color: white; padding-top: 32px; font-size:18px;'>Role</div>", unsafe_allow_html=True)
+    col_role.markdown(f"<div style='text-align: center; color: white; padding-top: 32px; font-size:18px;'>Role:</div>", unsafe_allow_html=True)
     role_input_slot = col_role_input.empty()
     st.session_state.user_role = role_input_slot.text_input('', '', key="role")
     
