6e83e749a87a5d3725c69959d7c6b742d1165c14
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2023-12-24 13:33:47 +0530

    Update plot_utils.py

--- plot_utils.py
+++ plot_utils.py
@@ -2,39 +2,39 @@ import plotly.graph_objects as go
 
 def plot_gauge_Balance(value):
             
-            current_price = value
-            ask_price = 100
-            bid_price = 50
-            spread = 5
-    
-            trace = go.Indicator(
-                    mode="gauge+number+delta",
-                    title={'text': "Balance"},
-                    delta={'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "#002b36"}},
-                    value=current_price,
-                    domain={'x': [0, 1], 'y': [0, 1]},
-                    gauge={
-                        'shape': 'angular',
-                        'axis': {'range': [bid_price - spread, ask_price + spread]},
-                        'bar': {'color': "black", 'thickness': 0.2},
-                        'bgcolor': 'black',
-                        'borderwidth': 2,
-                        'bordercolor': 'black',
-                        'steps': [
-                            {'range': [90, 100], 'color': 'green'},
-                            {'range': [85, 90], 'color': '#30F54B'},
-                            {'range': [80, 85], 'color': 'yellow'},
-                            {'range': [75, 80], 'color': 'orange'},
-                            {'range': [75, 50], 'color': 'red'}
-                        ],
-                        'threshold': {
-                            'line': {'color': 'blue', 'width': 6},
-                            'thickness': 0.75,
-                            'value': current_price,
-                        }
-                    }
-                )
-            return trace
+    current_price = value
+    ask_price = 100
+    bid_price = 50
+    spread = 5
+
+    trace = go.Indicator(
+        mode="gauge+number+delta",
+        title={'text': "Balance"},
+        delta={'reference': ask_price, 'relative': False, 'increasing': {'color': "RebeccaPurple"}, 'decreasing': {'color': "#002b36"}},
+        value=current_price,
+        domain={'x': [0, 1], 'y': [0, 1]},
+        gauge={
+            'shape': 'angular',
+            'axis': {'range': [bid_price - spread, ask_price + spread]},
+            'bar': {'color': "black", 'thickness': 0.2},
+            'bgcolor': 'black',
+            'borderwidth': 2,
+            'bordercolor': 'black',
+            'steps': [
+                {'range': [90, 100], 'color': 'green'},
+                {'range': [85, 90], 'color': '#30F54B'},
+                {'range': [80, 85], 'color': 'yellow'},
+                {'range': [75, 80], 'color': 'orange'},
+                {'range': [75, 50], 'color': 'red'}
+            ],
+            'threshold': {
+                'line': {'color': 'blue', 'width': 6},
+                'thickness': 0.75,
+                'value': current_price,
+            }
+        }
+    )
+    return trace
 
 def plot_gauge_APScale(value):
 
