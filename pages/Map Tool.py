027c7fd2991cc5852bc9e991e20c6903bced268b
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-02 18:49:30 +0530

    Create Map Tool.py

---
+++ pages/Map Tool.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import base64
+
+# Path to your HTML file with the Leaflet map
+html_file_path = 'map.html'
+
+# Path to your CSV file
+csv_file_path = 'US_cities_population.csv'
+
+# Read the contents of the CSV file and encode it to base64
+with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
+    csv_content = csv_file.read()
+    csv_base64 = base64.b64encode(csv_content.encode()).decode()
+
+# Embed the base64 encoded CSV content into the HTML
+with open(html_file_path, 'r', encoding='utf-8') as html_file:
+    html_content = html_file.read()
+    html_content = html_content.replace('fetch(\'US_cities_population.csv\')',
+                                        f'fetch(\'data:text/csv;base64,{csv_base64}\')')
+
+# Use Streamlit's components API to render the HTML, CSS, and JavaScript
+components.html(html_content, height=600)
