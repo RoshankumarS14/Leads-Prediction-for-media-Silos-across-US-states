ef327b70e621e0fbddfd54ab9c1aec0d1817d797
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-14 19:28:40 +0530

    Update map_utils.py

--- map_utils.py
+++ map_utils.py
@@ -39,7 +39,7 @@ def get_centre_zoom(json_data,states):
     if zoom_level<=3:
         zoom_level=3
 
-    # return center_lat,center_lon,zoom_level
-    return center_lat,center_lon,calculate_zoom_level(width, height, len(states))
+    return center_lat,center_lon,zoom_level
+    # return center_lat,center_lon,calculate_zoom_level(width, height, len(states))
 
 
