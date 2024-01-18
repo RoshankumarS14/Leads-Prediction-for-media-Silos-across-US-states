eef0765cb2161e0f37e331bc1ed80fd6995f0911
Roshankumar S <123869873+RoshankumarS14@users.noreply.github.com>
2024-01-18 22:38:28 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -417,7 +417,7 @@ if calculate:
     img = get_image(fig2,337,178,(66,66,650,420))
     sheet.add_image(img, 'C36')
     
-    img2 = get_image(fig2,337,178,(66,66,650,420))
+    img2 = get_image(fig2,337,188,(66,66,650,420))
     sheet2.add_image(img2, 'C36')
 
     state_df = st.session_state["state_df"]
@@ -438,7 +438,7 @@ if calculate:
     map_img = get_image(fig3,337,299)
     sheet.add_image(map_img, 'C20')
 
-    map_img2 = get_image(fig3,337,299)
+    map_img2 = get_image(fig3,337,315)
     sheet2.add_image(map_img2, 'C20')
 
     # Write DataFrame to Excel from cell X11 for the first column
